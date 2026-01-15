8149355e68faf30f701e700742d87275448fd3b0
km1031kim <km1031kim@naver.com>
2026-01-15 22:29:43 +0900

    배포

--- src/main/java/com/datastreams/hbase/hbaseputter/HbaseImagePutter.java
+++ src/main/java/com/datastreams/hbase/hbaseputter/HbaseImagePutter.java
@@ -14,6 +14,8 @@ import org.springframework.util.FileCopyUtils;
 import javax.annotation.PostConstruct;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @Component
 @RequiredArgsConstructor
@@ -36,13 +38,18 @@ public class HbaseImagePutter {
 
     private byte[] cachedImageBytes;
 
+    private static final int nThreads = 10;
+
+    private final ExecutorService executorService = Executors.newFixedThreadPool(nThreads);
+
     @PostConstruct
     public void init() throws IOException {
         // 스트림 열어서 바이트 배열로 변환
         try (InputStream is = imageResource.getInputStream()) {
             log.info("이미지 로딩 시작.");
             cachedImageBytes = FileCopyUtils.copyToByteArray(is);
-            log.info("이미지 메모리 로딩 끝. 크키 : " + cachedImageBytes.length + " bytes");
+
+            log.info("이미지 메모리 로딩 끝. 크기 : {} bytes", cachedImageBytes.length);
         } catch (IOException e) {
             throw new RuntimeException("이미지 로딩 실패", e);
         }
@@ -66,20 +73,32 @@ public class HbaseImagePutter {
     @Scheduled(fixedDelay = 1000)
     public void upload() {
         if (cachedImageBytes == null) return;
+        initializeThreads();
+    }
 
-        TableName tableName = TableName.valueOf(tableNameStr);
-        BufferedMutatorParams params = new BufferedMutatorParams(tableName).writeBufferSize(1024 * 1024 * 12);
-
-        try (BufferedMutator mutator = connection.getBufferedMutator(params)) {
-            String rowKey = "kjg_" + String.valueOf(System.currentTimeMillis());
-            Put put = new Put(Bytes.toBytes(rowKey));
-            put.addColumn(cf.getBytes(), column.getBytes(), cachedImageBytes);
-
-            mutator.mutate(put);
-            log.info("전송 완료");
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+    private void initializeThreads() {
+        for (int i = 1; i <= nThreads; i++) {
+            executorService.submit(new HbasePutter());
         }
-        log.info("전송 완료");
+    }
+
+    class HbasePutter implements Runnable {
+
+        @Override
+        public void run() {
+            TableName tableName = TableName.valueOf(tableNameStr);
+            BufferedMutatorParams params = new BufferedMutatorParams(tableName).writeBufferSize(1024 * 1024 * 12);
+
+            try (BufferedMutator mutator = connection.getBufferedMutator(params)) {
+                String rowKey = "kjg_" + System.currentTimeMillis();
+                Put put = new Put(Bytes.toBytes(rowKey));
+                put.addColumn(cf.getBytes(), column.getBytes(), cachedImageBytes);
+
+                mutator.mutate(put);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            log.info("전송 완료");
+        }
     }
 }
